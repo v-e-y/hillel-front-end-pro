facd17b8e49bedc3971fcdafc057b1ca5943eb41
Edward V <v-e-y@outlook.com>
2024-03-07 23:36:47 +0200

    fix function dataSet dependency

--- home-work/lesson-5/hw-6/index.js
+++ home-work/lesson-5/hw-6/index.js
@@ -4,16 +4,18 @@ const dataSet = require("./data-set");
 
 /**
  * Get values by condition
+ * @param {Array<Object>} dataResource
  * @param {String} propName
  * @param {Function} condition
  * @returns {Array<String|Boolean|Number>} values
  */
-function getValuesByCondition(propName, condition) {
-    return dataSet.filter(condition).map((item) => item[propName]);
+function getValuesByCondition(dataResource, propName, condition) {
+    return dataResource.filter(condition).map((item) => item[propName]);
 }
 
 console.log(
     getValuesByCondition(
+        dataSet,
         'phone',
         (item) => Number(item.balance.replace(/[^0-9\.-]+/g,"")) > 2000
     )
